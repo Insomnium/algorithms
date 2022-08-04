92b19fba46f920c5c72afcda92f66223b4a9d02c
Igor_Petrov <Igor_Petrov@epam.com>
2022-08-04 12:09:31 +0600

    Problem: Binary Tree Level Order Traversal // https://leetcode.com/problems/binary-tree-level-order-traversal
    Simplify and optimize a bit

--- src/main/java/net/ins/edu/algorithms/leetcode/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/net/ins/edu/algorithms/leetcode/BinaryTreeLevelOrderTraversal.java
@@ -10,56 +10,33 @@ import java.util.Map;
  */
 public class BinaryTreeLevelOrderTraversal {
 
-    private final Map<Integer, List<Entry>> map = new HashMap<>();
+    private final Map<Integer, List<Integer>> map = new HashMap<>();
 
     public List<List<Integer>> levelOrder(TreeNode root) {
-        traverseOrdered(root, 0, root);
+        traverseOrdered(root, 0);
 
         var result = new ArrayList<List<Integer>>(map.keySet().size());
 
         map.keySet().stream().sorted().forEach(level -> {
-            List<Entry> levelEntries = map.get(level);
-
-            List<Integer> tmpList = new ArrayList<>(1);
-            TreeNode prevParent = null;
-            for (Entry entry : levelEntries) {
-                if (prevParent == null || entry.parent.val != prevParent.val) {
-                    if (!tmpList.isEmpty())
-                        result.add(tmpList);
-                    tmpList = new ArrayList<>(2);
-                }
-                tmpList.add(entry.val);
-                prevParent = entry.parent;
-            }
-            if (!tmpList.isEmpty())
-                result.add(tmpList);
+            List<Integer> levelEntries = map.get(level);
+            result.add(new ArrayList<>(levelEntries.size()) {{ addAll(levelEntries); }});
         });
 
         return result;
     }
 
-    private void traverseOrdered(TreeNode node, int level, TreeNode parent) {
+    private void traverseOrdered(TreeNode node, int level) {
         if (node == null)
             return;
 
         if (node.left != null)
-            traverseOrdered(node.left, level + 1, node);
+            traverseOrdered(node.left, level + 1);
 
         if (node.right != null)
-            traverseOrdered(node.right, level + 1, node);
+            traverseOrdered(node.right, level + 1);
 
-        var levelValuesLeftToRight = map.computeIfAbsent(level, integer -> new ArrayList<>((int) Math.pow(2, level)));
-        levelValuesLeftToRight.add(new Entry(parent, node.val));
-    }
-
-    private class Entry {
-        private TreeNode parent;
-        private int val; // TODO: refactor: change to TreeNode for the universality
-
-        public Entry(TreeNode parent, int val) {
-            this.parent = parent;
-            this.val = val;
-        }
+        var levelValuesLeftToRight = map.computeIfAbsent(level, integer -> new ArrayList<>());
+        levelValuesLeftToRight.add(node.val);
     }
 
     public static class TreeNode {
@@ -82,9 +59,7 @@ public class BinaryTreeLevelOrderTraversal {
     }
 
     public static void main(String[] args) {
-//        var tree = new TreeNode(3, new TreeNode(9, null, null), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
-        var tree = new TreeNode(1, new TreeNode(2, new TreeNode(4), null), new TreeNode(3, null, new TreeNode(5)));
-        List<List<Integer>> lists = new BinaryTreeLevelOrderTraversal().levelOrder(tree);
-        System.out.println(lists);
+        System.out.println(new BinaryTreeLevelOrderTraversal().levelOrder(new TreeNode(3, new TreeNode(9, null, null), new TreeNode(20, new TreeNode(15), new TreeNode(7)))));
+        System.out.println(new BinaryTreeLevelOrderTraversal().levelOrder(new TreeNode(1, new TreeNode(2, new TreeNode(4), null), new TreeNode(3, null, new TreeNode(5)))));
     }
 }
