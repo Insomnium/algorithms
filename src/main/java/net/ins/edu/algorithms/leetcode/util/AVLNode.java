5f65ebbf7711586a9335b87351a31101c860a63b
Igor_Petrov <Igor_Petrov@epam.com>
2022-08-05 14:08:04 +0600

    refactor subtree height and balanceFactor calculation methods

--- src/main/java/net/ins/edu/algorithms/leetcode/util/AVLNode.java
+++ src/main/java/net/ins/edu/algorithms/leetcode/util/AVLNode.java
@@ -25,8 +25,7 @@ public class AVLNode<T extends Comparable<T>> extends TreeNode<T> {
         AVLNode<T> n = (AVLNode<T>) super.insert(node, val);
 
         // recursion ended (going upwards the tree)
-        n.height = 1 + max(nodeHeight((AVLNode<T>) n.left), nodeHeight((AVLNode<T>) n.right));
-        n.balanceFactor = nodeBalanceFactor(n);
+        n.recalculateAttributes();
 
         // left left case
         if (n.balanceFactor > 1 && val.compareTo(n.left.val) < 0) {
@@ -52,6 +51,16 @@ public class AVLNode<T extends Comparable<T>> extends TreeNode<T> {
         return n;
     }
 
+    private AVLNode<T> recalculateAttributes() {
+        this.height = 1 + max(nodeHeight((AVLNode<T>) this.left), nodeHeight((AVLNode<T>) this.right));
+        this.balanceFactor = nodeHeight((AVLNode<T>) this.left) - nodeHeight((AVLNode<T>) this.right);
+        return this;
+    }
+
+    private int nodeHeight(AVLNode<T> node) {
+        return node == null ? 0 : node.height;
+    }
+
     private AVLNode<T> rightRotate(AVLNode<T> y) {
         AVLNode<T> x = (AVLNode<T>) y.left;
         AVLNode<T> T2 = (AVLNode<T>) x.right;
@@ -59,11 +68,8 @@ public class AVLNode<T extends Comparable<T>> extends TreeNode<T> {
         x.right = y;
         y.left = T2;
 
-        y.height = 1 + max(nodeHeight((AVLNode<T>) y.left), nodeHeight((AVLNode<T>) y.right));
-        x.height = 1 + max(nodeHeight((AVLNode<T>) x.left), nodeHeight((AVLNode<T>) x.right));
-
-        x.balanceFactor = nodeBalanceFactor(x);
-        y.balanceFactor = nodeBalanceFactor(y);
+        y.recalculateAttributes();
+        x.recalculateAttributes();
 
         return x;
     }
@@ -75,21 +81,10 @@ public class AVLNode<T extends Comparable<T>> extends TreeNode<T> {
         x.left = y;
         y.right = T2;
 
-        y.height = 1 + max(nodeHeight((AVLNode<T>) y.left), nodeHeight((AVLNode<T>) y.right));
-        x.height = 1 + max(nodeHeight((AVLNode<T>) x.left), nodeHeight((AVLNode<T>) x.right));
-
-        x.balanceFactor = nodeBalanceFactor(x);
-        y.balanceFactor = nodeBalanceFactor(y);
+        y.recalculateAttributes();
+        x.recalculateAttributes();
 
         return x;
-    }
-
-    private int nodeBalanceFactor(AVLNode<T> node) {
-        return node == null ? 0 : (nodeHeight((AVLNode<T>) node.left) - nodeHeight((AVLNode<T>) node.right));
-    }
-
-    private int nodeHeight(AVLNode<T> node) {
-        return node == null ? 0 : node.height;
     }
 
     @SuppressWarnings("unchecked")
