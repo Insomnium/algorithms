2d561cb8fd6f4520b7593f8746b3861786b798c4
igorp <IgVPetrov@inno.tech>
2022-08-28 22:48:21 +0600

    add accumulated changes

--- src/main/kotlin/net/ins/edu/algorithms/search/leetcode/LongestSubstringWithoutRepeatingCharacters.kt
+++ src/main/kotlin/net/ins/edu/algorithms/search/leetcode/LongestSubstringWithoutRepeatingCharacters.kt
@@ -4,35 +4,49 @@ class Solution {
 
     fun lengthOfLongestSubstring(s: String): Int {
         var result = ""
+        var curIdx = 0
+        val idx = 0
+        var curCandidateLen = 0
 
-        s.forEachIndexed { index, c ->
-            val candidate = moveOn(s, index)
-            result = if (candidate.length > result.length) candidate else result
-        }
+        do {
+            val candidate = moveOn(s, curIdx + idx - curCandidateLen + 1)
+            curIdx += candidate.first // wrong
+            val res = candidate.second
+            result = if (res.length > result.length) res else result
+            curCandidateLen = result.length
+            if (candidate.first < 0) break
+        } while (curIdx <= s.length)
 
         return result.length
     }
 
-    fun moveOn(s: String, from: Int = 0): String {
+    fun moveOn(s: String, from: Int = 0): Pair<Int, String> {
         var str = ""
-        var previous = mutableSetOf<Char>()
+        val previous = mutableSetOf<Char>()
+        if (s.length <= 1) return -1 to s
 
         s.substring(from).forEachIndexed { index, c ->
             if (c in previous) {
-                return str
+                return index to str
             } else {
                 previous.add(c)
                 str += c
             }
         }
-        return str
+        return -1 to str
     }
 }
 
 fun main() {
-//    println(Solution().lengthOfLongestSubstring("abcabcbb"))
-//    println(Solution().lengthOfLongestSubstring("bbbbb"))
-//    println(Solution().lengthOfLongestSubstring("pwwkew"))
+    println(Solution().lengthOfLongestSubstring("abcabcbb"))
+    println(Solution().lengthOfLongestSubstring("bbbbb"))
+    println(Solution().lengthOfLongestSubstring("pwwkew"))
     println(Solution().lengthOfLongestSubstring("a"))
     println(Solution().lengthOfLongestSubstring(" "))
+    println(Solution().lengthOfLongestSubstring("dvdf"))
+    println(Solution().lengthOfLongestSubstring("anviaj"))
+    println(Solution().lengthOfLongestSubstring(""))
+    println(Solution().lengthOfLongestSubstring("au"))
+    println(Solution().lengthOfLongestSubstring("aud"))
+    println(Solution().lengthOfLongestSubstring("audi"))
 }
