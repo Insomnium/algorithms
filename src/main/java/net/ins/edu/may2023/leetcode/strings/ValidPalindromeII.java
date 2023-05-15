be485f02874936979c8e4c0db6cef031e1e8d22f
Igor Petrov <Igor_Petrov@epam.com>
2023-05-15 23:39:42 +0600

    new May 2023 run

---
+++ src/main/java/net/ins/edu/may2023/leetcode/strings/ValidPalindromeII.java
@@ -0,0 +1,32 @@
+package net.ins.edu.may2023.leetcode.strings;
+
+/**
+ * <a href="https://leetcode.com/problems/valid-palindrome/">125. Valid Palindrome</a>
+ */
+public class ValidPalindromeII {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isPalindrome("aba")); // true
+    }
+
+    static class Solution {
+
+        public boolean isPalindrome(String s) {
+            if (s == null) return false;
+            if (s.isBlank()) return true;
+
+            var chars = sanitize(s).toCharArray();
+            for (int i = 0, j = chars.length-1; i <= j; i++, j--) {
+                if (chars[i] != chars[j]) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private String sanitize(String s) {
+            return s.replaceAll("[^A-Za-z0-9]+", "").toLowerCase();
+        }
+    }
+}
